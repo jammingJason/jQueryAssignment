229f68ed6b6142732fa59e6d473b42d06e2edac3
Van Jason Crews <vanjasoncrews@gmail.com>
2022-08-10 08:18:41 -0400

    completed files

--- movie.js
+++ movie.js
@@ -3,16 +3,47 @@ $(document).ready(function () {
     '<table cellspacing=15><tr class=ratings><td>Movie Title</td><td>Movie Rating</td></tr>'
   );
 });
-
-$('button').on('click', function (evt) {
+let objMovie = {};
+let arrMoviesList = [];
+$('#btnSubmit').on('click', function (evt) {
   evt.preventDefault();
-  $('table').last().append('<tr class=ratings>');
+  let strID = Math.random();
+  strID = strID.toString().replace('0.', '');
+  const movieLength = $('#txtMovieTitle').get(0).value.toString().length;
+  const movieRating = Number($('#txtMovieRating').get(0).value);
 
+  if (movieLength < 2) {
+    alert('Movie title must be at least 2 characters');
+    return;
+  }
+
+  if (movieRating < 1 || movieRating > 10 || isNaN(movieRating)) {
+    alert('Movie rating must be between 1 - 10');
+    $('#txtMovieRating').get(0).value = '';
+    return;
+  }
+
+  $('table')
+    .last()
+    .append('<tr id=' + strID + ' class=ratings>');
   const intLength = $('.movieRating').get().length;
   for (let i = 0; i < intLength; i++) {
     $('.ratings')
       .last()
-      .append('<td>' + $('.movieRating').get(i).value) + '</td>';
+      .append('<td>' + $('.movieRating').get(i).value + '</td>');
   }
-  $('tr').last().append('<td><button>Remove this Rating</button></td>');
+
+  let strBtnID = Math.random();
+  strBtnID = strBtnID.toString().replace('0.', '');
+  $('tr')
+    .last()
+    .append('<td><button id=' + strBtnID + '>Remove this Rating</button></td>')
+    .on('click', function (evt) {
+      evt.preventDefault();
+      $(this).remove();
+    });
+  let inputLength = $('input').get().length;
+  for (let i = 0; i < inputLength; i++) {
+    $('input').get(i).value = '';
+  }
 });
